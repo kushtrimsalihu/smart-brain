505c77c5ad5c9c9203796217779a391eea7bb950
kushtrimsalihu <38229104+kushtrimsalihu@users.noreply.github.com>
2020-10-06 06:41:29 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin= require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        connectString: process.env.DATABASE_URL,
+        ssl: true,
+    }
+});
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/',(req,res)=>{res.send('it is working')})
+
+
+app.post('/signin', (req,res)=>{signin.handleSignin(req ,res , db , bcrypt)})
+
+
+
+app.post('/register',(req,res)=> {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id',(req,res )=>{
+    msWriteProfilerMark.handleProfileGet(req,res,db)
+})
+
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+
+app.post('/imageurl ', (req, res) => {
+    image.handleApiCall(req, res, )
+})
+
+
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+console.log("app is running on port ${process.enc.PORT}")
+});
+
+/*
+
+/--> res=this is working
+/signin -->n POST  succses/fail
+/register--> POST =  user
+/profile/:userId-->GET = user
+/image --> PUT --> user
+*/
